f33a71058afa48c3b6bbc30d2865c3166965e483
Derrick Beining <derrick.beining@gmail.com>
2017-11-05 23:33:41 -0500

    rotateMatrix90() tests and solution added

--- arrays-strings/arrays-strings.solution.js
+++ arrays-strings/arrays-strings.solution.js
@@ -1,60 +1,12 @@
-export const hasNoRepeatChars = str => {
-  if (typeof str !== 'string') throw new TypeError('strings only please');
-  if (!str.length) return true;
-  for (let i = 0; i < str.length - 1; i++) {
-    const currChar = str[i].toLowerCase();
-    for (let j = i + 1; j < str.length; j++) {
-      const laterChar = str[j].toLowerCase();
-      if (currChar === laterChar) return false;
-    }
-  }
-  return true;
-}
-
-export const isPermutation = (str1, str2) => {
-  // if strings have diff lengths, they can't be permutations
-  if (str1.length !== str2.length) return false;
-  // loop through first string
-  for (let i = 0; i < str1.length; i++) {
-    // for each letter in first string, loop through second string
-    for (let j = 0; j < str2.length; j++) {
-      // stop inner loop and increment outer loop whenever a match is found
-      if (str1[i] === str2[j]) break;
-      // if you reach the last letter of inner loop without a match,
-      // it's not a permutation
-      if ((j === str2.length - 1) && str1[i] !== str2[j]) {
-        return false;
-      }
-    }
-  }
-  // if every letter in outer loop finds a match, must be a permutation
-  return true;
-}
-
-export const reverse = str => {
-  const reversed = [];
-  for (let i = str.length - 1 ; i >= 0; i--) {
-    reversed.push(str[i]);
-  }
-  return reversed.join('');
-}
-
-export const urlEncodeSpaces = str => {
-  const stringBuffer = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === ' ') stringBuffer.push('%20');
-    else stringBuffer.push(str[i]);
-  }
-  return stringBuffer.join('');
-}
-
 const interleave = (arr1, arr2) => {
   const merged = [];
-  const longer = arr1.length > arr2.length ? arr1 : arr2;
+  const longer = arr1.length > arr2.length
+    ? arr1
+    : arr2;
   for (let i = 0; i < longer.length; i++) {
     if (arr1[i]) merged.push(arr1[i]);
     if (arr2[i]) merged.push(arr2[i]);
-  }
+    }
   return merged;
 }
 
@@ -77,4 +29,89 @@ export const compress = str => {
   const compressed = interleave(charsFound, charCounts).join('');
   if (compressed.length < str.length) return compressed;
   else return str;
+  }
+
+export const hasNoRepeatChars = str => {
+  if (typeof str !== 'string') throw new TypeError('strings only please');
+  if (!str.length) return true;
+  for (let i = 0; i < str.length - 1; i++) {
+    const currChar = str[i].toLowerCase();
+    for (let j = i + 1; j < str.length; j++) {
+      const laterChar = str[j].toLowerCase();
+      if (currChar === laterChar) return false;
+      }
+    }
+  return true;
+}
+
+export const isPermutation = (str1, str2) => {
+  // if strings have diff lengths, they can't be permutations
+  if (str1.length !== str2.length) return false;
+
+  // loop through first string
+  for (let i = 0; i < str1.length; i++) {
+    // for each letter in first string, loop through second string
+    for (let j = 0; j < str2.length; j++) {
+      // stop inner loop and increment outer loop whenever a match is found
+      if (str1[i] === str2[j]) break;
+
+      // if you reach the last letter of inner loop without a match, it's not a
+      // permutation
+      if ((j === str2.length - 1) && str1[i] !== str2[j]) {
+        return false;
+      }
+    }
+  }
+  // if every letter in outer loop finds a match, must be a permutation
+  return true;
+}
+
+export const reverse = str => {
+  const reversed = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed.push(str[i]);
+  }
+  return reversed.join('');
+}
+
+export const rotateMatrix90 = matrix => {
+  console.log(matrix);
+  // last idx for any row/col:
+  const ultima = matrix.length - 1;
+  // num concentric squares to rotate:
+  const totalConcentricSquares = Math.floor(matrix.length / 2);
+  let numSquaresRotated = 0;
+    while (numSquaresRotated < totalConcentricSquares) {
+      // idx to access top or left side:
+      const first = numSquaresRotated;
+      // idx to access right or bottom side:
+      const last = ultima - numSquaresRotated;
+        // idx to traverse top or left side:
+      for (let firstOffset = first; firstOffset < last; firstOffset++) {
+        // idx to traverse right or bottom side:
+        const ultimaOffset = ultima - firstOffset;
+        // store top:
+        let temp = matrix[first][firstOffset];
+        // top <-- left
+        matrix[first][firstOffset] = matrix[ultimaOffset][first];
+        // left <-- bottom
+        matrix[ultimaOffset][first] = matrix[last][ultimaOffset];
+        // bottom <-- right
+        matrix[last][ultimaOffset] = matrix[firstOffset][last];
+        // right <-- top
+        matrix[firstOffset][last] = temp;
+      }
+      numSquaresRotated++;
+    }
+  console.log(matrix);
+  return matrix;
+}
+
+export const urlEncodeSpaces = str => {
+  const stringBuffer = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') stringBuffer.push('%20');
+    else stringBuffer.push(str[i]);
+    }
+  return stringBuffer.join('');
 }
---
+++ arrays-strings/rotateMatrix.spec.js
@@ -0,0 +1,60 @@
+/* eslint-disable new-cap  */
+import {expect} from 'chai';
+import {rotateMatrix90} from './arrays-strings';
+
+describe('function rotateMatrix()', () => {
+
+  const NxNMatrix = num => {
+    const matrix = Array(num).fill(0).map(() => Array(num));
+    let count = 0;
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        matrix[i][j] = ++count;
+      }
+    }
+    return matrix;
+  }
+
+  let matrix3x3;
+  beforeEach('make new matrix', () => {
+    matrix3x3 = NxNMatrix(3);
+  });
+
+  it('takes an NxN two-dimensional array and returns a reference to the same array', () => {
+    let result = rotateMatrix90(matrix3x3);
+    expect(result).to.be.an.instanceOf(Array);
+    expect(matrix3x3 === result).to.be.true;
+  })
+
+  it('rotates the elements of the passed-in matrix clock-wise 90 degrees IN PLACE (i.e. without creating a new array)', () => {
+    /* Example:
+            this  ------ becomes ---->  this
+      [1,  2,  3,  4],              [13,  9, 5, 1],
+      [5,  6,  7,  8],              [14, 10, 6, 2],
+      [9,  10, 11, 12],             [15, 11, 7, 3],
+      [13, 14, 15, 16]              [16, 12, 8, 4]
+    */
+    const unrotated = matrix3x3.map(row => [...row]);
+    const length = matrix3x3.length;
+    rotateMatrix90(matrix3x3)
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length; j++) {
+        expect(unrotated[i][j]).to.equal(matrix3x3[j][length - 1 - i])
+      }
+    }
+  })
+
+  it('can rotate an NxN matrix of any size', () => {
+    for (let num = 4; num < 10; num++) {
+      const currMatrix = NxNMatrix(num);
+      const unrotated = currMatrix.map(row => [...row]);
+      const length = currMatrix.length;
+      rotateMatrix90(currMatrix)
+      for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+          expect(unrotated[i][j]).to.equal(currMatrix[j][length - 1 - i])
+        }
+      }
+    }
+  })
+})
